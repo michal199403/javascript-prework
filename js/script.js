479c91e4b8ac722f3ec1e0919565f2204ad17e14
Haraburda Michał <mich1l199403@gmail.com>
2020-05-03 20:18:05 +0200

    Add bootstrap and first styles

--- js/script.js
+++ js/script.js
@@ -57,11 +57,11 @@ document.getElementById('play-rock').addEventListener('click', function(){
     playGame(1);
   });
 
-  document.getElementById('play-paper').addEventListener('click', function(){
+document.getElementById('play-paper').addEventListener('click', function(){
     playGame(2);
   });
 
-  document.getElementById('play-scissors').addEventListener('click', function(){
+document.getElementById('play-scissors').addEventListener('click', function(){
     playGame(3);
   });
 
